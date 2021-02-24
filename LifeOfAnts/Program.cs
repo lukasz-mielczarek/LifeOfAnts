c39f78fbcc16762a36b23a1f12a52ecf67f7ee75
lukasz-mielczarek <lukasz.mielczarek1989@gmail.com>
2021-02-24 15:06:37 +0100

    all done

---
+++ LifeOfAnts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LifeOfAnts
+{
+    public class Program
+    {
+        private HiveMap _map;
+        private int roundCounter;
+
+        public static void Main()
+        {
+            new Program();
+        }
+
+        private Program()
+        {
+            PopulateMap test1 = new PopulateMap();
+            //foreach(string test111 in test1.allFieldsType)
+            //{
+            //    Console.WriteLine(test111);
+            //}
+            //_map = new HiveMap(203,203);
+
+        }
+    }
+}
